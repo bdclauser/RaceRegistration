a0cb549b054a3d442e3bcb4754f367ccce57acfc
Brian <13192476+bdclauser@users.noreply.github.com>
2022-04-13 22:17:44 -0400

    initial Commit

---
+++ RaceRegistration.js
@@ -0,0 +1,13 @@
+let raceNumber = Math.floor(Math.random() * 1000);
+const age = 19;
+var registeredEarly = 'true';
+
+if (registeredEarly = 'true' && age > 18){
+  console.log(`Race at 9:30 am, your Race number: ${raceNumber}`);
+} else if (registeredEarly != 'true' && age > 18){
+  console.log(`Race at 11:00 am, your Race number: ${raceNumber}`);
+} else if (age < 18) {
+  console.log('Race at 12:30, your Race number: ${raceNumber}');
+} else {
+  console.log('runner, see the registration desk');
+}
